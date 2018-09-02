4588fa6466c3657a37274d54f45f369e05b4230d
Sarmad <sarmadsa2@gmail.com>
2018-09-02 21:17:42 +0200

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+        char[] A = {'a','b','c'};
+        char[] B = {'a','c','d'};
+
+        FunctionsCalc f = new FunctionsCalc();
+    }
+}
