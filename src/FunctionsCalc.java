5f57fa13de524256c59cbbabeb04d17a352ae227
Sarmad <sarmadsa2@gmail.com>
2018-09-03 23:05:22 +0200

    Implement count possible one to one

--- src/FunctionsCalc.java
+++ src/FunctionsCalc.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class FunctionsCalc {
 
+    public void countPossibleInjections(char[] inputArr, char[] outputArr){
+        HashMap<Character,Character> mapper = new HashMap<>();
+        //ArrayList<char[]> componitions = new ArrayList<>();
+        //int count = 0;
+        int possibleFunction = (inputArr.length * outputArr.length);
+        for(char element: inputArr){
+            for(char val: outputArr){
+                mapper.put(element, val);
+                System.out.println("[" + element + " , " + val + "]");
+            }
+        }
+        System.out.println("Number of possible Injections: " + possibleFunction);
+        //return count;
+    }
 
+    public void countPossibleOnto(char[] inputArr, char[] outputArr){
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
 
     public static void main(String[] args){
-        char[] A = {'a','b','c'};
-        char[] B = {'a','c','d'};
+        char[] A = {'1','2','3'};
+        char[] B = {'1','2','3'};
 
         FunctionsCalc f = new FunctionsCalc();
+        f.countPossibleInjections(A,B);
     }
 }
